29c82b370a2782ce39b20508634b29e666ec8021
cuardin <daniel@armyr.se>
2017-06-24 22:16:24 +0200

    Now stores each run into a csv file

--- daemon/stdaemon.py
+++ daemon/stdaemon.py
@@ -1,20 +1,79 @@
+from __future__ import print_function
 import subprocess
 import json
+import sys
+import os
+from datetime import datetime
+from datetime import tzinfo
 
-proc = subprocess.Popen( './speedtest-cli --json', shell=True, stdout=subprocess.PIPE );
+import sys
 
-output = proc.communicate()
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-result = output[0]
-print ( '-------' )
+#-------------------
+# Function to parse json result returned from speedtest-cli
+def parseSTOutput ( resultStr ):
+	try: 
+		jsonObj = json.loads( resultStr )
+
+		downloadMbps = jsonObj['download']/(1024.0*1024)
+		uploadMbps = jsonObj['upload']/(1024.0*1024)	
+		timestamp = datetime.strptime( jsonObj['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ' )
+	except ValueError:
+		eprint ( "Error parsing SpeedTest result:")
+		eprint ( resultStr )
+		downloadMbps = float('nan')
+		uploadMbps = float('nan')
+		timestamp = float('nan')
+	
+	return ( {'timestamp': timestamp, 'downloadMbps': downloadMbps, 'uploadMbps': uploadMbps } )	
+
+#-------------------
+# Function to call speedtest-cli and get json response
+def doSpeedtest():
+	if ( 0 ):
+		try:		
+			proc = subprocess.Popen( ['./speedtest-cli', '--json'], stdout=subprocess.PIPE );		
+			result = proc.communicate()						
+			stString = result[0]
+		except:
+			eprint ( "Error calling speedtest-cli" )
+			stString = ""
+	else:
+		stString = '{"bytes_sent": 2678784, "download": 11046830.089912508, "timestamp": "2017-06-24T18:35:33.742058Z", "share": null, "bytes_received": 13884164, "ping": 23.668, "upload": 1346336.9976509183, "server": {"latency": 23.668, "name": "Stockholm", "url": "http://speedtest1.dchosting.se/speedtest/upload.php", "country": "Sweden", "lon": "18.0686", "cc": "SE", "host": "speedtest1.dchosting.se:8080", "sponsor": "Datacom", "url2": "http://speedtest2.dchosting.se/speedtest/upload.php", "lat": "59.3294", "id": "8424", "d": 195.76265185298232}}'
+	return stString
+
+#-------------------
+# Function that calls speedtest-cli and parses result
+def doSpeedTestAndParse():
+	stString = doSpeedtest()
+	result = parseSTOutput( stString )
+	return result
+
+#-----------------
+# Function to write a piece of data to the log file
+def writeAMeasurementResult( result ):
+	fileName = './measurementLog.txt'
+
+	outputString = "{}\t{}\t{}\n".format( result["timestamp"].strftime('%Y-%m-%dT%H:%M:%S.%fZ'), result["downloadMbps"], result["uploadMbps"] )	
+	
+	if (os.path.isfile( fileName) == False):
+		fout = open( fileName, 'w')
+		fout.write( 'timestamp\tdownloadMbps\tuploadMbps\n' )
+		fout.close()
+	
+	fout = open( fileName, 'a')	
+	fout.seek(0,os.SEEK_END)
+	fout.write( outputString )
+	fout.close()
+
+
+
+#-----------------
+# Start of main program
+
+result = doSpeedTestAndParse()
+writeAMeasurementResult( result )
 print ( result )
-print ( '-------' )
 
-#output = '{"bytes_sent": 2564096, "download": 10982638.579192186, "timestamp": "2017-06-23T19:51:42.012954Z", "share": null, "bytes_received": 13843204, "ping": 23.178, "upload": 1706722.617285763, "server": {"latency": 23.178, "name": "Kista", "url": "http://kst5-speedtest-1.tele2.net/speedtest/upload.php", "country": "Sweden", "lon": "17.9444", "cc": "SE", "host": "kst5-speedtest-1.tele2.net:8080", "sponsor": "Tele2", "url2": "http://kst5-speedtest-1.tele2.net/upload.php", "lat": "59.4014", "id": "6061", "d": 185.1072773722867}}'
-jsonObj = json.loads(result)
-print json.dumps( jsonObj, sort_keys=True, indent=4, separators=(',', ': '))
-
-print ( '-------' )
-print (  str(jsonObj['download']) + " / " + str(jsonObj['upload']) )
-print ( '-------' )
-print ( jsonObj['timestamp'])
